808e67e1fe5082bc326f71839d24932692fe65b0
Caio <whoiscaio@protonmail.com>
2023-02-17 09:21:54 -0300

    feat: gitignore preferences updated

--- dist/functions/textConvert.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function textConvert(text) {
-    var newText = text.replace(/-/g, ' ');
-    return newText.charAt(0).toUpperCase() + newText.substring(1);
-}
-exports["default"] = textConvert;
